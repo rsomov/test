f21d60b6f03b891fd0ac55caef0682203bede5ca
roman <rsomov@gmail.com>
2018-01-31 14:26:16 +0200

    remove db service

--- src/main/java/ee/rosman/github/test/Application.java
+++ src/main/java/ee/rosman/github/test/Application.java
@@ -1,28 +1,12 @@
 package ee.rosman.github.test;
 
-import ee.rosman.github.test.service.DbService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-
-    private final DbService dbService;
-    private final ApplicationContext appContext;
-
-    public Application(DbService dbService, ApplicationContext appContext) {
-        this.dbService = dbService;
-        this.appContext = appContext;
-    }
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    public void run(String[] args) throws Exception {
-        dbService.runScripts(appContext.getResources("classpath:db/*.js"));
     }
 }
